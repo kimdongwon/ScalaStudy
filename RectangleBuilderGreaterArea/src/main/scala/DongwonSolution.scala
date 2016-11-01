533a1d218d705fabf63f7d89949ac8836eb3a87f
DongWon.Kim <congcain@naver.com>
2016-11-01 22:05:24 +0900

    알고리즘 풀이 완료

--- RectangleBuilderGreaterArea/src/main/scala/DongwonSolution.scala
+++ RectangleBuilderGreaterArea/src/main/scala/DongwonSolution.scala
@@ -4,6 +4,32 @@
   */
 object DongwonSolution {
   def solution(a: Array[Int], x: Int): Int = {
-    2
+    def getNumberCountTuple(v:Array[Int]) :Map[Long,Int] = v.map(_.toLong).groupBy(identity).mapValues(_.length)
+
+    val numberCountMap = getNumberCountTuple(a)
+    val useNumbers  = (Array() ++ numberCountMap.filter(_._2 >= 2).keySet).sorted
+
+    var possibleValue = useNumbers.filter(numberCountMap(_) >= 4).reverse.takeWhile(v => v*v >= x).length
+    var possibleIndex = -1
+    for (i <- 1 until useNumbers.length){
+      var startIndex = 0
+      if(possibleIndex == -1){
+        startIndex = i
+      }else{
+        startIndex = possibleIndex
+      }
+
+      val count = (startIndex-1 to 0 by -1).takeWhile(useNumbers(_) * useNumbers(i) >= x).length
+      if(count > 0){
+        possibleIndex = startIndex - count
+      }
+      if(possibleIndex != -1){
+        possibleValue = possibleValue + i - possibleIndex
+        if(possibleValue >= 1000000000){
+          -1
+        }
+      }
+    }
+    possibleValue
   }
 }
--- RectangleBuilderGreaterArea/src/test/scala/DongwonSolutionTest.scala
+++ RectangleBuilderGreaterArea/src/test/scala/DongwonSolutionTest.scala
@@ -1,10 +1,13 @@
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * Created by dongwon on 2016-10-28.
   */
 class DongwonSolutionTest extends FlatSpec with Matchers {
   it should "Solution Test" in {
     DongwonSolution.solution(Array(1,2,5,1,1,2,3,5,1), 5) should be (2)
+    DongwonSolution.solution(Array(6, 6, 6, 6, 6, 6), 10) should be (1)
   }
 }
