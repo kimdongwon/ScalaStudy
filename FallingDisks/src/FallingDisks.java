d5591107557061c57b07ff9a604a6311f00c0951
alwaysholiday <steadyeffort@gmail.com>
2016-11-23 13:49:05 +0900

    FallingDisk 폴더 생성, 솔루션 업로드

---
+++ FallingDisks/src/FallingDisks.java
@@ -0,0 +1,10 @@
+public class FallingDisks {
+	public static void main(String[] args){
+		int[] A = {5,6,4,3,6,2,3};
+		int[] B = {2,3,5,2,4};
+//		int[] A = {1,1};
+//		int[] B = {1};
+		Solution solution = new Solution();
+		System.out.println(solution.solution(A, B));
+	}
+}
